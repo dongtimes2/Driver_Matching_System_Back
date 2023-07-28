a98ab5f5f7d2c5ec47d2c4a4df2a7518d2c07758
dongtimes2 <dongtimes2@gmail.com>
2023-07-29 04:06:02 +0900

    feat: 라우팅 기능 구현

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Application } from "express";
 import { PORT } from "./config/secretKey.js";
 import loaders from "./loaders/index.js";
+import router from "./routes/router.js";
+import createHttpError from "http-errors";
 
 const startServer = async () => {
   const app: Application = express();
 
   await loaders(app);
+
+  app.use(router);
+
+  app.use((req, res, next) => {
+    next(createHttpError(404));
+  });
 
   app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
