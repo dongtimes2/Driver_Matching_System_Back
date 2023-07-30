469617dde0ab9e509fd3ad045ae711986a7114cb
dongtimes2 <dongtimes2@gmail.com>
2023-07-30 18:30:28 +0900

    feat: patch accounts 라우팅 및 로직 구현

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -7,7 +7,7 @@ router.get("/", async (req, res, next) => {
   const uid = req.uidData;
 
   try {
-    const userData = await User.findById(uid);
+    const userData = await User.findById(uid).lean();
     res.json({
       name: userData?.name,
       type: userData?.type,
@@ -15,6 +15,24 @@ router.get("/", async (req, res, next) => {
   } catch (error) {
     console.error(error);
     return next(new Error());
+  }
+});
+
+router.patch("/", async (req, res, next) => {
+  const { type } = req.body;
+  const uid = req.uidData;
+
+  if (type === "driver" || type === "passenger") {
+    try {
+      await User.findByIdAndUpdate(uid, { type });
+    } catch (error) {
+      console.error(error);
+      next(new Error());
+    }
+  } else {
+    return res.status(400).json({
+      message: "올바르지 않은 타입이 전달되었음",
+    });
   }
 });
 
