517272017a143865b2ff081e4526b4572c4be300
dongtimes2 <dongtimes2@gmail.com>
2023-08-03 03:34:23 +0900

    feat: app.ts에서 소켓 통신부 주입

--- src/app.ts
+++ src/app.ts
@@ -4,21 +4,23 @@ import loaders from "./loaders/index.js";
 import router from "./routes/router.js";
 import createHttpError from "http-errors";
 import errorHandler from "./middlewares/errorHandler.js";
+import http from "http";
+
+import socketModule from "./loaders/socket.js";
 
 const startServer = async () => {
   const app: Application = express();
+  const httpServer = http.createServer(app);
 
   await loaders(app);
-
+  await socketModule(httpServer);
   app.use(router);
-
   app.use((req, res, next) => {
     next(createHttpError(404));
   });
-
   app.use(errorHandler);
 
-  app.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
   });
 };
